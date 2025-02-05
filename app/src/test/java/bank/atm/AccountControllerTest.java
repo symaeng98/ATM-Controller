b773c6a7ae796361910da840115d3f975a8ae8c2
맹순영 <symaeng98@naver.com>
2025-02-05 19:13:12 +0900

    test: 폴더 구조 변경 및 Account 테스트 작성

---
+++ app/src/test/java/bank/atm/AccountControllerTest.java
@@ -0,0 +1,105 @@
+package bank.atm;
+
+import bank.atm.database.Database;
+import bank.atm.domain.account.controller.AccountController;
+import bank.atm.domain.account.dto.AccountCreate;
+import bank.atm.domain.account.entity.Account;
+import bank.atm.domain.account.repository.AccountRepository;
+import bank.atm.domain.account.repository.MemoryAccountRepository;
+import bank.atm.domain.account.service.AccountService;
+import bank.atm.domain.card.dto.CardCreate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AccountControllerTest {
+    private Database database;
+
+    private AccountController accountController;
+
+    @BeforeEach
+    void setUp() {
+        database = new Database();
+
+        // 계좌 의존성 주입
+        AccountRepository accountRepository = new MemoryAccountRepository(database);
+        AccountService accountService = new AccountService(accountRepository);
+        accountController = new AccountController(accountService);
+
+        CardCreate cardCreate = new CardCreate("mycard1", "내 첫 카드", "1234");
+        database.createCard(cardCreate.toCard());
+    }
+
+    @Test
+    void createAccountSuccess() {
+        AccountCreate accountCreate = new AccountCreate("mycard1", "월급 계좌");
+
+        accountController.createAccount(accountCreate);
+        List<Account> accounts = database.getAllAccountsByCardId("mycard1");
+
+        Account account = accounts.get(0);
+        assertEquals("mycard1", account.getCardId());
+        assertEquals("월급 계좌", account.getDescription());
+        assertEquals(0, account.getBalance()); //생성 직후 잔액은 0이어야 합니다.
+    }
+
+    @Test
+    void getAccountsByCardId() {
+        AccountCreate accountCreate1 = new AccountCreate("mycard1", "월급 계좌");
+        AccountCreate accountCreate2 = new AccountCreate("mycard1", "월급 계좌2");
+        database.createAccount(accountCreate1.toAccount());
+        database.createAccount(accountCreate2.toAccount());
+
+        List<Account> accounts = accountController.getAccountsByCardId("mycard1");
+
+        assertEquals(2, accounts.size());
+        assertEquals("mycard1", accounts.get(0).getCardId());
+        assertEquals("월급 계좌", accounts.get(0).getDescription());
+        assertEquals(0, accounts.get(0).getBalance());
+        assertEquals("mycard1", accounts.get(1).getCardId());
+        assertEquals("월급 계좌2", accounts.get(1).getDescription());
+        assertEquals(0, accounts.get(1).getBalance());
+    }
+
+    @Test
+    void depositSuccess() {
+        AccountCreate accountCreate = new AccountCreate("mycard1", "월급 계좌");
+        database.createAccount(accountCreate.toAccount());
+        List<Account> accounts = database.getAllAccountsByCardId("mycard1");
+        Account account = accounts.get(0);
+        String accountId = account.getId();
+
+        accountController.deposit(accountId, 1000);
+
+        assertEquals(1000, account.getBalance());
+    }
+
+    @Test
+    void withdrawSuccess() {
+        AccountCreate accountCreate = new AccountCreate("mycard1", "월급 계좌");
+        database.createAccount(accountCreate.toAccount());
+        List<Account> accounts = database.getAllAccountsByCardId("mycard1");
+        Account account = accounts.get(0);
+        String accountId = account.getId();
+
+        accountController.deposit(accountId, 10000);
+        accountController.withdraw(accountId, 3000);
+
+        assertEquals(7000, account.getBalance());
+    }
+
+    @Test
+    void withdrawFail_NotEnoughBalance() {
+        AccountCreate accountCreate = new AccountCreate("mycard1", "월급 계좌");
+        database.createAccount(accountCreate.toAccount());
+        List<Account> accounts = database.getAllAccountsByCardId("mycard1");
+        Account account = accounts.get(0);
+        String accountId = account.getId();
+
+        assertThrows(IllegalStateException.class, () -> accountController.withdraw(accountId, 1000));
+    }
+}
--- app/src/test/java/bank/atm/CardControllerTest.java
+++ app/src/test/java/bank/atm/CardControllerTest.java
@@ -14,37 +14,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class ATMControllerTest {
+public class CardControllerTest {
     private Database database;
     private CardController cardController;
-    private CardService cardService;
-    private CardRepository cardRepository;
 
     @BeforeEach
     void setUp() {
         database = new Database();
 
-        // 카드 초기화
-        cardRepository = new MemoryCardRepository(database);
-        cardService = new CardService(cardRepository);
+        // 카드 의존성 주입
+        CardRepository cardRepository = new MemoryCardRepository(database);
+        CardService cardService = new CardService(cardRepository);
         cardController = new CardController(cardService);
-
-        // 계좌 초기화
-//        cardRepository = new MemoryAccountRepository(database);
-//        cardService = new CardService(cardRepository);
-//        cardController = new CardController(cardService);
     }
 
-
-    /**
-     * 카드 관련 테스트 (Controller, Service, Repository)
-     */
     @Test
     void createCardSuccess() {
-        CardCreate card = new CardCreate("mycard1", "내 첫 카드", "1234");
-        cardController.createCard(card);
+        CardCreate cardCreate = new CardCreate("mycard1", "내 첫 카드", "1234");
 
-        Card foundCard = database.getCard("mycard1");
+        cardController.createCard(cardCreate);
+        Card foundCard = database.getCardById("mycard1");
 
         assertEquals("mycard1", foundCard.getId());
         assertEquals("내 첫 카드", foundCard.getDescription());
@@ -53,26 +42,22 @@ public class ATMControllerTest {
     @Test
     void createCardFail_AlreadyExists() {
         CardCreate cardCreate = new CardCreate("mycard1", "내 첫 카드", "1234");
-        cardController.createCard(cardCreate);
+        database.createCard(cardCreate.toCard());
 
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> cardController.createCard(new CardCreate("mycard1", "잘못된 카드 id", "4521"))
-        );
+        assertThrows(IllegalArgumentException.class,
+                () -> cardController.createCard(new CardCreate("mycard1", "잘못된 카드 id", "4521")));
     }
 
     @Test
     void createCardFail_WrongPINNumber() {
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> new CardCreate("mycard1", "내 첫 카드", "125789")
-        );
+        assertThrows(IllegalArgumentException.class,
+                () -> new CardCreate("mycard1", "내 첫 카드", "125789"));
     }
 
     @Test
     void getCardSuccess() {
-        CardCreate card = new CardCreate("mycard1", "내 첫 카드", "1234");
-        cardController.createCard(card);
+        CardCreate cardCreate = new CardCreate("mycard1", "내 첫 카드", "1234");
+        database.createCard(cardCreate.toCard());
 
         Card foundCard = cardController.getCard("mycard1");
 
@@ -82,14 +67,10 @@ public class ATMControllerTest {
 
     @Test
     void verifyPinFail() {
-        CardCreate card = new CardCreate("mycard1", "내 첫 카드", "1234");
-        cardController.createCard(card);
-
+        CardCreate cardCreate = new CardCreate("mycard1", "내 첫 카드", "1234");
+        database.createCard(cardCreate.toCard());
         CardVerify verify = new CardVerify("mycard1", "4321");
 
-        assertThrows(
-                IllegalArgumentException.class,
-                () -> cardController.verifyPinNumber(verify)
-        );
+        assertThrows(IllegalArgumentException.class, () -> cardController.verifyPinNumber(verify));
     }
 }
