a8d669f20c371764ab78a77b99aa3a1bf004227a
맹순영 <symaeng98@naver.com>
2025-02-05 15:16:23 +0900

    test: PIN 번호 관련 테스트

--- app/src/test/java/bank/atm/ATMControllerTest.java
+++ app/src/test/java/bank/atm/ATMControllerTest.java
@@ -3,6 +3,7 @@ package bank.atm;
 import bank.atm.database.Database;
 import bank.atm.domain.card.controller.CardController;
 import bank.atm.domain.card.dto.CardCreate;
+import bank.atm.domain.card.dto.CardVerify;
 import bank.atm.domain.card.entity.Card;
 import bank.atm.domain.card.repository.CardRepository;
 import bank.atm.domain.card.repository.MemoryCardRepository;
@@ -79,5 +80,16 @@ public class ATMControllerTest {
         assertEquals("내 첫 카드", foundCard.getDescription());
     }
 
+    @Test
+    void verifyPinFail() {
+        CardCreate card = new CardCreate("mycard1", "내 첫 카드", "1234");
+        cardController.createCard(card);
 
+        CardVerify verify = new CardVerify("mycard1", "4321");
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> cardController.verifyPinNumber(verify)
+        );
+    }
 }
