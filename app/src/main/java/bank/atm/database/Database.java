02b67260aa76fc6fb1f5f1a684b61b8fa552727b
맹순영 <symaeng98@naver.com>
2025-02-05 19:09:03 +0900

    feat: Account관련 database 구현

--- app/src/main/java/bank/atm/database/Database.java
+++ app/src/main/java/bank/atm/database/Database.java
@@ -4,6 +4,7 @@ import bank.atm.domain.account.entity.Account;
 import bank.atm.domain.card.entity.Card;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,14 +15,40 @@ public class Database {
     private Map<String, Account> accountDb = new HashMap<>();
     private Map<String, Card> cardDb = new HashMap<>();
 
-    public void createCard(String id, Card card) {
+    public void createAccount(Account account) {
+        String id = account.getId();
+        if (accountDb.containsKey(id)) {
+            throw new IllegalArgumentException("이미 존재하는 계좌 id입니다.");
+        }
+        accountDb.put(id, account);
+    }
+
+    public Account getAccountById(String id) {
+        if (!accountDb.containsKey(id)) {
+            throw new IllegalArgumentException("id에 해당하는 계좌가 없습니다.");
+        }
+        return accountDb.get(id);
+    }
+
+    public List<Account> getAllAccountsByCardId(String cardId) {
+        if (!cardDb.containsKey(cardId)) {
+            throw new IllegalArgumentException("id에 해당하는 계좌가 없습니다.");
+        }
+
+        return accountDb.values().stream()
+                .filter(account -> account.getCardId().equals(cardId))
+                .toList();
+    }
+
+    public void createCard(Card card) {
+        String id = card.getId();
         if (cardDb.containsKey(id)) {
             throw new IllegalArgumentException("이미 존재하는 카드 id입니다.");
         }
         cardDb.put(id, card);
     }
 
-    public Card getCard(String id) {
+    public Card getCardById(String id) {
         if (!cardDb.containsKey(id)) {
             throw new IllegalArgumentException("id에 해당하는 카드가 없습니다.");
         }
